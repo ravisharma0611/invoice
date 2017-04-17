f237fe86fb8080b829c9e92d70c9c6e238da6ab3
RAVI <RAVI@RAVI-PC>
2017-04-18 01:30:48 +0530

    Adding Login classes

---
+++ InvoiceService/src/com/login/action/LoginAction.java
@@ -0,0 +1,23 @@
+package com.login.action;
+
+import com.invoice.action.BaseServiceAction;
+
+public class LoginAction extends BaseServiceAction {
+
+	public String loginUser(){
+		
+		return null;
+	}
+	
+	private boolean validateUser(){
+		
+		return false;
+	}
+	
+	@Override
+	public void validate() {
+		super.validate();
+		
+		
+	}
+}
---
+++ InvoiceService/src/com/login/bean/RegisterUserBean.java
@@ -0,0 +1,66 @@
+package com.login.bean;
+
+import java.util.Date;
+
+public class RegisterUserBean {
+
+	private String firstName;
+	private String lastName;
+	
+	private String userName;
+	private String password;
+	
+	private String emailId;
+	private long phoneNumber;
+	private Date dateOfBirth;
+	private String gender;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
